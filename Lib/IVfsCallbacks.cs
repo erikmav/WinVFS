db1d89421c06355020d1d9f4478a0b936c25f4b8
Erik Mavrinac <erikmav@microsoft.com>
2024-02-05 10:50:04 -0800

    Code cleanup

--- Lib/IVfsCallbacks.cs
+++ Lib/IVfsCallbacks.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System;
+﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Threading;
--- Tests/VfsIntegrationTests.cs
+++ Tests/VfsIntegrationTests.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
